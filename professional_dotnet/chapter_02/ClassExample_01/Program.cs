c7553ba7f27dae984f98d14bd9e3b09215a7d3f3
Srushti <srushtibattise@gmail.com>
2024-10-22 09:38:15 +0530

    Adding the projects "ClassExample_01" and "ClassWithConstructor_01" in the solution "chapter_02"

---
+++ professional_dotnet/chapter_02/ClassExample_01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Program for creating a simple class in C# with Visual Studio
+
+// Programmer:Srushti Battise
+
+// Defining a class named Car
+public class Car
+{
+    //Field:Data that the Car holds
+
+    //color of the car
+    public string color;
+    //speed of the car
+    public int speed;
+
+    //Method:An action that the car can perfor,
+    public void Drive()
+    {
+        //This method will print a message when called
+        Console.WriteLine("The car is Driving at speed" + "speed" + "km/h");
+    }
+}
+
+namespace ClassExample_01
+{
+    //Using the Car class to create an object
+    public class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            //Create an Object of the Car class
+            Car myCar = new Car();
+            //This is like building a new robot from the blueprint
+            //Assign values to the fields of the Object
+            myCar.color = "red";
+            myCar.speed = 100;
+            //Call the Method to perfoem the action
+            myCar.Drive();
+            //The Output:The car Driving at 100 km/h.
+
+        }
+
+    }
+}
---
+++ professional_dotnet/chapter_02/ClassWithConstructor_01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Program for creating class with constructor in C# with Visual Studio
+
+// Programmer:Srushti Battise
+
+//Defining a class named Person
+public class Person
+{
+    //Field:Data That the Person Holds;
+    public string name;
+    public int age;
+    //Constructor:Special Method to initialise Person's Data
+    public Person(string personName, int personAge)
+    {
+        name = personName;
+        //set the name field
+        age = personAge;
+        //set the age field
+    }
+    //Method:CALL TO SHOW PERSON'S DETAILS
+    public void greet()
+    {
+        Console.WriteLine("Hello,I am " + "name" + "age" + "years old.");
+    }
+}
+
+namespace ClassWithConstructor_01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+           //create an object of the person class using class constructor
+           Person student = new Person ("ALICE" , 16);
+            //CALL THE GREET METHOD
+            student.greet();
+            //THE OUTPUT:Hello,I am ALICE  and I am 16 Years Old.
+        }
+    }
+}
