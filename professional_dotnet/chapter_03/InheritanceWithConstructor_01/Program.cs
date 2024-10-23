d605501ce918a22f0b1c4f4738b6203e11e58e30
Srushti <srushtibattise@gmail.com>
2024-10-23 11:26:11 +0530

    adding a new project "InheritanceWith Constructor_01" in solution "chapter_03"

---
+++ professional_dotnet/chapter_03/InheritanceWithConstructor_01/Program.cs
@@ -0,0 +1,40 @@
+﻿// Program to create Inheritance with Constructor using C# in Visual Studio
+// Programmer:Srushti Battise
+
+// Base class
+public class Vehicle
+{
+    public string Brand;
+
+    // Constructor 
+    public Vehicle(string brand)
+    
+    {
+        Brand = brand;
+    }
+
+    public void ShowBrand()
+    {
+        Console.WriteLine("Vehicle Brand:" + Brand);
+    }
+}
+//Derived Class
+public class Car : Vehicle
+{
+    public Car(string brand) : base(brand) { }
+}
+
+namespace InheritanceWithConstructor_01 { }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program to create Inheritance with Constructor using C# in Visual Studio\n");
+
+            Car myCar = new Car("Toyota");
+            // Output:Vehicle Brand:Toyota
+            myCar.ShowBrand();
+        }
+    }
+
