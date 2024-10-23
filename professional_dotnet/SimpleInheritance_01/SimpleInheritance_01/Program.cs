9a8d10fd212a79b87c59c7b82761d111c7a89e86
Srushti <srushtibattise@gmail.com>
2024-10-23 10:25:04 +0530

    added a new project "SimpleInheritance_01" to the solution "chapter_03"

---
+++ professional_dotnet/SimpleInheritance_01/SimpleInheritance_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Base class
+public class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("This animal is eating.");
+    }
+}
+
+// Derived class
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("The dog is barking.");
+    }
+}
+
+namespace SimpleInheritance_01 {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Eat();  // Output: This animal is eating.
+            myDog.Bark(); // Output: The dog is barking.
+        }
+    }
+}
