e0c2af345f53e286db9ff72889f64da126e4f91e
srushti-battise-programmer-mumbai <srushtibattise@gmail.com>
2024-11-18 01:29:39 +0530

    adding new projects to solution "chapter_03".

---
+++ professional_dotnet/chapter_02/ClassWithProperties(Advanced Fields)_01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Program to create  Class With Properties Advanced Fields in C# with visual studio
+// Programmer:Srushti Battise
+namespace ClassWithPropertiesAdvancedFields_01
+{
+    // Defining a class named "Book"
+    public class Book
+    {
+        // Property: Like a field, but with more control
+        private string title; // Private field, cannot be accessed directly
+
+        // Public property to get and set the value of the title
+        public string Title
+        {
+            get { return title; } // Getter method
+            set { title = value; } // Setter method
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an object of the Book class
+            Book myBook = new Book();
+
+            // Set the title using the property
+            myBook.Title = "The Adventures of Sherlock Holmes";
+
+            // Get and print the title using the property
+            Console.WriteLine("Book title: " + myBook.Title); // Output: The Adventures of Sherlock Holmes
+        }
+    }
+
+}
---
+++ professional_dotnet/chapter_02/Inheritance -OneClassInheritingfromAnother_01/Program.cs
@@ -0,0 +1,45 @@
+﻿// Program to create Inheritance - One Class Inheriting from Another in C# with visual studio
+// Programmer:Srushti Battise
+
+namespace InheritanceOneClassInheritingfromAnother_01 
+    {
+    // Defining a base class named "Animal"
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        // Method in the base class
+        public void Eat()
+        {
+            Console.WriteLine(Name + " is eating.");
+        }
+    }
+
+    // Defining a derived class named "Dog" that inherits from "Animal"
+    public class Dog : Animal
+    {
+        // Additional method specific to the Dog class
+        public void Bark()
+        {
+            Console.WriteLine(Name + " is barking.");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program to create Inheritance - One Class Inheriting from Another in C# with visual studio\r\n");
+            // Create an object of the Dog class
+            Dog myDog = new Dog();
+
+            // Set the dog's name
+            myDog.Name = "Buddy";
+
+            // Call methods from both the base class and the derived class
+            myDog.Eat();  // Output: Buddy is eating. (Inherited from Animal class)
+            myDog.Bark(); // Output: Buddy is barking. (Defined in Dog class)
+        }
+    }
+
+}
